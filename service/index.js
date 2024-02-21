96d3f78a961f7952f764c87c27010fd21d26df99
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-02-21 13:34:00 +0530

    SERVICE: updated

--- service/index.js
+++ service/index.js
@@ -21,13 +21,17 @@ async function runCommand(command) {
 async function installDocker() {
   try {
     await runCommand("curl --version");
+    await runCommand("ls");
     await runCommand("curl -fsSL https://get.docker.com -o get-docker.sh");
+    await runCommand("ls");
     await runCommand("sh get-docker.sh");
     await runCommand("rm get-docker.sh");
     await runCommand(`usermod -aG docker root`);
     await runCommand("systemctl enable docker");
     await runCommand("systemctl start docker");
     await runCommand("docker compose up -d");
+    await runCommand("ls");
+    await runCommand("docker --version");
 
     console.log("Docker installed successfully!");
   } catch (error) {
